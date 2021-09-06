d072cdef82903ad2e7ced4e226ce69bfe8103278
Katz <mykatz01@gmail.com>
2021-09-05 20:45:30 -0700

    Add Tunes to derivatives list

--- utils/lists.ts
+++ utils/lists.ts
@@ -372,4 +372,9 @@ export const derivativesList: Record<string, string>[] = [
     description: "Decentralized, composable money for the metaverse",
     url: "https://opensea.io/collection/treasure-for-loot",
   },
+  {
+    name: "Tunes",
+    description: "Unique 8bit songs for every Loot holder",
+    url: "https://www.loottunes.fun/",
+  },
 ];
