4d53aeae68c4eb006bf96c1a869ad60180967e41
Vie <yangchenzhong@gmail.com>
2021-09-09 10:16:11 +0800

    add comma

--- utils/lists.ts
+++ utils/lists.ts
@@ -465,6 +465,6 @@ export const derivativesList: Record<string, string>[] = [
   {
     name: "Loot Land",
     description: "Loot Land are parts of the world in Loot metaverse",
-    url: "https://opensea.io/collection/landproject"
+    url: "https://opensea.io/collection/landproject",
   }
 ];
