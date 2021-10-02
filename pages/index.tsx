b2654cc2f773b39bc31aae6622f188e5c84edaf0
pondering democritus <ponderingdemocritus@protonmail.com>
2021-10-03 08:22:38 +1100

    add homepage links

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,23 +16,31 @@ export default function Home(): ReactElement {
 
   return (
     <Layout>
-      <div className="container mx-auto px-4 text-center py-40 justify-around flex">
+      <div className="container mx-auto px-4 text-center py-40 justify-around flex flex-wrap">
         <div className="sm:w-1/2">
           <h1>Loot</h1>
           <p className="text-2xl">Loot is randomized adventurer gear generated and stored on chain.
           Stats, images, and other functionality are intentionally omitted for others to interpret.
 Feel free to use Loot in any way you want.</p>
         </div>
+        <div className="flex w-full justify-around mt-8">
+          <div className="flex text-2xl">
+            <button className="bg-black mx-4 p-4">Marketplaces</button>
+            <button className="bg-black mx-4 p-4">Communities</button>
+            <button className="bg-black mx-4 p-4">Resources</button>
+            <button className="bg-black mx-4 p-4">Guilds</button>
+          </div>
+        </div>
 
       </div>
       <div>
         <div className="bg-black py-20">
           <div className="container mx-auto mt-8">
-            <div className="flex justify-around">
+            <div className="flex justify-around -mt-40">
               <div className="bg-gray-800 px-20 py-8 rounded text-center">
                 <span className="uppercase text-xl font-semibold">The Quest begins here</span>
                 <h1 className="text-center mt-4">Chapter 1: Finding the Bags</h1>
-                <p className="text-2xl">8000 Bags were discovered which unleashed the creativity of thousands</p>
+                <p className="text-2xl">8000 Bags were discovered which unleashed the creativity of thousands. </p>
               </div>
             </div>
 
